76e9ba96708f5290e41b6e0317ec0fc43391eb24
Anton5r <antondobryj5044@gmail.com>
2023-04-03 01:20:53 +0200

    add png; red scss

--- pages/about/about.js
+++ pages/about/about.js
@@ -1,4 +1,10 @@
 import styles from '@/styles/Home.module.css'
+import pic from '@/public/tfy-1.jpg'
+import pic2 from '@/public/tfy-2.jpg'
+import pic3 from '@/public/tfy-3.jpg'
+import pic4 from '@/public/tfy-4.jpg'
+import Image from 'next/image'
+
 
 export default function About () {
     return (
@@ -15,10 +21,12 @@ export default function About () {
               </div>
           </div>
         </header>
-        
         <h2 className={styles.wrapper}>Тут пример работы и объяснение формул</h2>
 
-
+        <Image className={styles.img} src={pic} alt='Error PNG' width={600} height={750}/>
+        <Image className={styles.img} src={pic2} alt='Error PNG' width={600} height={750}/>
+        <Image className={styles.img} src={pic3} alt='Error PNG' width={600} height={750}/>
+        <Image className={styles.img} src={pic4} alt='Error PNG' width={600} height={500}/>
 
         </>
     )
