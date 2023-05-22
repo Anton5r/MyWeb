6433c4a5c64563fb47c77ddefc3f96e9b938007a
Anton5r <antondobryj5044@gmail.com>
2023-05-22 09:13:40 +0500

    asd

--- pages/posts.js
+++ pages/posts.js
@@ -26,16 +26,14 @@ export default function Posts () {
 
         <div className={styles.linkp}>
         <h3>Сделано</h3>
-        <p>
+        <p className='sizefile'>
         <a className={styles.file} href="https://docs.google.com/document/d/1c1kyZ7sHxYRE2h2hdAwwGW0ZrrVf40k1/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">1) Лаба</a><br/>
         <a className={styles.file} href="https://docs.google.com/document/d/1C_Tg_KowpE2da9BZKeBZL4KlnNcYIv_D/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">2) Лаба</a><br/>
         <a className={styles.file} href="https://docs.google.com/document/d/1-8rJjW9gaKo2x5N2S0FFzLngMD84TPae/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">3) Лаба</a><br/>
         <a className={styles.file} href="https://docs.google.com/document/d/1g8jJ_YblKxb6B8h228F3YTPf2ALEJ73J/edit?usp=share_link&ouid=106894929838315556260&rtpof=true&sd=true">5) Лаба</a><br/>
         <a className={styles.file} href="https://docs.google.com/document/d/1R1EjYaSkg-8r5ZvzNP-p15Xc_-fP8L8Z/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">7) Лаба</a><br/>
-        </p>
-        <p className='sizefile'>
-            <a className={styles.file} href="https://docs.google.com/document/d/1c--nub_9k0iS1UgOBBe39FimcPPF6s0g/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">4) Лаба</a><br/>
-            <a className={styles.file} href="https://docs.google.com/document/d/1OGuz5PwZkJcjZOu6dWMm7gETeY8J4aqO/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">6) Лаба</a><br/>
+        <a className={styles.file} href="https://docs.google.com/document/d/1c--nub_9k0iS1UgOBBe39FimcPPF6s0g/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">4) Лаба</a><br/>
+        <a className={styles.file} href="https://docs.google.com/document/d/1OGuz5PwZkJcjZOu6dWMm7gETeY8J4aqO/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">6) Лаба</a><br/>
         </p>
 
         <h3>Оформление должно быть по госту</h3>
