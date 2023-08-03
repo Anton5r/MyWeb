cbba890d770b3ffe5aa9aa27075f095670f0a357
Anton5r <antondobryj5044@gmail.com>
2023-08-03 21:41:14 +0500

    mass-ok

--- pages/test.js
+++ pages/test.js
@@ -8,7 +8,7 @@ export default function ContactUs() {
   function sendEmail(e) {
     e.preventDefault();    //This is important, i'm not sure why, but the email won't send without it
 
-    emailjs.sendForm('service_71p2n6i', 'template_waw3tbc', e.target, 'XUi4bOe33oI4zGpmI')
+    emailjs.sendForm('service_71p2n6i', 'template_dexjqgl', e.target, 'XUi4bOe33oI4zGpmI')
       .then((result) => {
           window.location.reload()  //This is if you still want the page to reload (since e.preventDefault() cancelled that behavior) 
       }, (error) => {
@@ -26,18 +26,17 @@ export default function ContactUs() {
       </Head>
 
       <h2 className={styles.text}>Педложение по обновление сайта</h2>
+      <h3>Так как он ещё в разработке может не корректно работать</h3>
 
     <form className="contact-form" onSubmit={sendEmail}>
       <input type="hidden" name="contact_number" />
-      <label>Name</label>
-      <input type="text" name="from_name" />
-      <label>Email</label>
-      <input type="email" name="from_email" />
-      <label>Subject</label>
-      <input type="text" name="subject" />
-      <label>Message</label>
-      <textarea name="html_message" />
-      <input type="submit" value="Send" />
+      <label className={styles.formin}>Имя</label>
+      <input type="text" name="from_name" /><br/>
+      <label className={styles.formin}>Почта</label>
+      <input type="email" name="from_email" className={styles.formin_lab}/><br/>
+      <label className={styles.formin}>Сообщение</label>
+      <textarea name="message" className={styles.formin_lab}/><br/>
+      <input type="submit" value="Отправить" className={styles.formin_lab}/>
     </form>
     </>
   )
