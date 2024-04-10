f265b1e57a49874f85dded751fb4203ffc009053
Anton5r <75029720+Anton5r@users.noreply.github.com>
2024-04-10 09:18:17 +0500

    asqwascsd

--- pages/posts.js
+++ pages/posts.js
@@ -15,17 +15,30 @@ export default function Posts () {
         <div className={styles.linkp}>
         <h3>Сделано</h3>
         <p className='sizefile'>
-        <a className={styles.file} href="https://docs.google.com/document/d/1c1kyZ7sHxYRE2h2hdAwwGW0ZrrVf40k1/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">1) Лаба</a><br/>
-        <a className={styles.file} href="https://docs.google.com/document/d/1C_Tg_KowpE2da9BZKeBZL4KlnNcYIv_D/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">2) Лаба</a><br/>
-        <a className={styles.file} href="https://docs.google.com/document/d/1-8rJjW9gaKo2x5N2S0FFzLngMD84TPae/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">3) Лаба</a><br/>
-        <a className={styles.file} href="https://docs.google.com/document/d/1g8jJ_YblKxb6B8h228F3YTPf2ALEJ73J/edit?usp=share_link&ouid=106894929838315556260&rtpof=true&sd=true">5) Лаба</a><br/>
-        <a className={styles.file} href="https://docs.google.com/document/d/1R1EjYaSkg-8r5ZvzNP-p15Xc_-fP8L8Z/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">7) Лаба</a><br/>
-        <a className={styles.file} href="https://docs.google.com/document/d/1c--nub_9k0iS1UgOBBe39FimcPPF6s0g/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">4) Лаба</a><br/>
-        <a className={styles.file} href="https://docs.google.com/document/d/1OGuz5PwZkJcjZOu6dWMm7gETeY8J4aqO/edit?usp=sharing&ouid=106894929838315556260&rtpof=true&sd=true">6) Лаба</a><br/>
+        <a className={styles.file} href="https://docs.google.com/document/d/1xndMF1PLf684ZFMb6WslKLbQk0P6NSx-/edit?usp=drive_link&ouid=106894929838315556260&rtpof=true&sd=true">1) Лаба</a><br/>
+        <a className={styles.file} href="https://docs.google.com/document/d/1FvTaCjzMJn1wBZZhC44SSpZvp1uCK5G0/edit?usp=drive_link&ouid=106894929838315556260&rtpof=true&sd=true">2) Лаба</a><br/>
+        <a className={styles.file} href="https://docs.google.com/document/d/18muqI4vWe-Jcu5u6AV6HhPAoRuc7JHBC/edit?usp=drive_link&ouid=106894929838315556260&rtpof=true&sd=true">3) Лаба</a><br/>
+        <a className={styles.file} href="#">4) Лаба</a><br/>
+        <a className={styles.file} href="#">5) Лаба</a><br/>
+        <a className={styles.file} href="#">6) Лаба</a><br/>
+        <a className={styles.file} href="#">7) Лаба</a><br/>
         </p>
 
         <h3>Оформление должно быть по госту</h3>
         <p className={styles.sizefile}><a className={styles.file} href="https://drive.google.com/file/d/1F9eU9P3wFFdsCjc41-pVF4U7P2CMz4rP/view?usp=sharing">ГОСТ по файлам</a></p>
+
+        <h3>У кого нет ворда то вот файл с русофикацией</h3>
+        <p><a className={styles.sizefile} href='https://www.microsoft.com/en-us/download/details.aspx?id=49117'>Сам файл</a></p>
+        <h3>Инструкция</h3>
+        <iframe 
+            scr = "https://drive.google.com/file/d/1GKXyZ1XUv8ZB9HGLEYUtY_tlaRwc0vvp/view?usp=drive_link"
+            frameborder="0"
+            allowfullscreen
+            className={styles.sizefile}
+        />
+        <p className={styles.sizefile}>Комманды: </p>
+        <p className={styles.sizefile}>reg add "HKCU\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\Ecs" /v "CountryCode" /t REG_SZ /d "std::wstring|US" /f</p>
+        <p className={styles.sizefile}>.\setup.exe /configure .\configuration-Office2021Enterprise.xml</p>
 
         </div>
                 
