9acab1e10e9f7950b62011041ee277475dd3ed50
Anton5r <antondobryj5044@gmail.com>
2024-04-10 10:34:17 +0500

    98289fheru9nvr

--- pages/posts.js
+++ pages/posts.js
@@ -20,6 +20,8 @@ export default function Posts () {
         <a className={styles.file} href="https://docs.google.com/document/d/1xndMF1PLf684ZFMb6WslKLbQk0P6NSx-/edit?usp=drive_link&ouid=106894929838315556260&rtpof=true&sd=true">1) Лаба</a><br/>
         <a className={styles.file} href="https://docs.google.com/document/d/1FvTaCjzMJn1wBZZhC44SSpZvp1uCK5G0/edit?usp=drive_link&ouid=106894929838315556260&rtpof=true&sd=true">2) Лаба</a><br/>
         <a className={styles.file} href="https://docs.google.com/document/d/18muqI4vWe-Jcu5u6AV6HhPAoRuc7JHBC/edit?usp=drive_link&ouid=106894929838315556260&rtpof=true&sd=true">3) Лаба</a><br/>
+        </p>
+        <p className={styles.file}>
         <a className={styles.file} href="#">4) Лаба</a><br/>
         <a className={styles.file} href="#">5) Лаба</a><br/>
         <a className={styles.file} href="#">6) Лаба</a><br/>
@@ -39,9 +41,9 @@ export default function Posts () {
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen
         ></iframe>
-        {/* <p className={styles.sizefile}>Комманды: </p>
+        <p className={styles.sizefile}>Комманды: </p>
         <p className={styles.sizefile}>reg add "HKCU\Software\Microsoft\Office\16.0\Common\ExperimentConfigs\Ecs" /v "CountryCode" /t REG_SZ /d "std::wstring|US" /f</p>
-        <p className={styles.sizefile}>.\setup.exe /configure .\configuration-Office2021Enterprise.xml</p> */}
+        <p className={styles.sizefile}>.\setup.exe /configure .\configuration-Office2021Enterprise.xml</p>
 
         </div>
                 
